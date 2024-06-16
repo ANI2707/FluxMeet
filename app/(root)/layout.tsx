1c8e7b8b9247bca33dbefa88814b34df4dd31063
Aniket <bhosaleaniket826@gmail.com>
2024-06-16 17:59:05 +0530

    meeting room created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,13 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
-
+export const metadata: Metadata = {
+  title: "Fluxmeet",
+  description: "Group Video calling app",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
     <main>
