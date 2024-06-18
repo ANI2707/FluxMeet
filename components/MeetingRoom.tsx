ba7824999c61f8712e5cd8a3a0a7c9e1d5b44e12
Aniket <bhosaleaniket826@gmail.com>
2024-06-18 15:03:28 +0530

    Personal meeting page done

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -6,22 +6,21 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LayoutList, Loader, Users } from "lucide-react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import EndCallButton from "./EndCallButton";
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
   const searchParams=useSearchParams()
-  // !!searchParams becoz we have to convert string value to boolean value
+  // !!searchParams becoz we have to convert string type value to boolean type value
   // if 'personal'=>!'personal'=>false=>!false=>true
   // if undefined => ! undefined => true => !true => false
-
+  const router=useRouter()
   const isPersonalRoom=!!searchParams.get('personal');
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants,setShowParticipants]=useState(false);
@@ -47,14 +46,14 @@ const MeetingRoom = () => {
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full item-center justify-center">
         <div className="flex size-full  max-w-[1000px] items-center">
-          <CallLayout/>
+          <CallLayout />
         </div>
         <div className={cn("h-[calc(100vh-86px)] hidden ml-2",{'show-block':showParticipants})}>
           <CallParticipantsList onClose={()=>setShowParticipants(false)}/>
         </div>
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls/>
+        <CallControls onLeave={()=>{router.push('/')}}/>
 
         <DropdownMenu>
           <div className="flex items-center">
@@ -81,7 +80,7 @@ const MeetingRoom = () => {
             <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"><Users size={20} className="text-white"/></div>
             
           </button>
-          {!isPersonalRoom && <EndCallButton/>}
+          {isPersonalRoom && <EndCallButton/>}
 
       </div>
     </section>
